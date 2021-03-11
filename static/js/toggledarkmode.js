d052ac3fd98bcf580a29663bddd543efc753624f
flaport <floris.laporte@gmail.com>
2021-03-11 13:13:21 +0100

    update github pages

--- static/js/toggledarkmode.js
+++ static/js/toggledarkmode.js
@@ -7,15 +7,18 @@ if (currentTheme) {
   document.documentElement.setAttribute("data-theme", currentTheme);
   if (currentTheme === "dark") {
     toggleSwitch.checked = true;
+    document.getElementById("logo").src = "/static/img/logo-white.svg";
   }
 }
 
 function switchTheme(e) {
   if (e.target.checked) {
     document.documentElement.setAttribute("data-theme", "dark");
+    document.getElementById("logo").src = "/static/img/logo-white.svg";
     localStorage.setItem("theme", "dark");
   } else {
     document.documentElement.setAttribute("data-theme", "light");
+    document.getElementById("logo").src = "/static/img/logo.svg";
     localStorage.setItem("theme", "light");
   }
 }
